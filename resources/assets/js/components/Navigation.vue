da0bb24fd1205403a1f1e7d237ce59e2dee29232
drdlee <danielrudi.lee@gmail.com>
2018-02-02 13:43:36 +0800

    Registering Home and Timeline component

--- resources/assets/js/components/Navigation.vue
+++ resources/assets/js/components/Navigation.vue
@@ -1,49 +1,48 @@
 <template>
   <nav class="navbar navbar-default navbar-static-top">
     <div class="container">
-      <div class="navbar-header">
+        <div class="navbar-header">
 
-          <!-- Collapsed Hamburger -->
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
-              <span class="sr-only">Toggle Navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-          </button>
+            <!-- Collapsed Hamburger -->
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
+                <span class="sr-only">Toggle Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
 
-          <!-- Branding Image -->
-          <a class="navbar-brand" href="#">
-              Boilerplate
-          </a>
-      </div>
+            <!-- Branding Image -->
+            <router-link :to="{ name: 'home' }" class="navbar-brand">Boilerplate</router-link>
+        </div>
 
-      <div class="collapse navbar-collapse" id="app-navbar-collapse">
-          <!-- Left Side Of Navbar -->
-          <ul class="nav navbar-nav">
-              &nbsp;
-          </ul>
+        <div class="collapse navbar-collapse" id="app-navbar-collapse">
+            <!-- Left Side Of Navbar -->
+            <ul class="nav navbar-nav">
+                &nbsp;
+            </ul>
 
-          <!-- Right Side Of Navbar -->
-          <ul class="nav navbar-nav navbar-right">
-              <!-- Authentication Links -->
-             
-              <li><router-link :to="{ name: 'login' }">Login</router-link></li>
-              <li><a href="#">Register</a></li>
-          
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-                    Rudi Lee <span class="caret"></span>
-                </a>
+            <!-- Right Side Of Navbar -->
+            <ul class="nav navbar-nav navbar-right">
+                <!-- Authentication Links -->
+                
+                <li><router-link :to="{ name: 'timeline' }">Timeline</router-link></li>
+                <li><router-link :to="{ name: 'login' }">Login</router-link></li>
+                <li><router-link :to="{ name: 'register' }">Register</router-link></li>
+            
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
+                        Rudi Lee <span class="caret"></span>
+                    </a>
 
-                <ul class="dropdown-menu">
-                  <li>
-                    <a href="#">Logout</a>
-                  </li>
-                </ul>
-              </li>
-             
-          </ul>
-      </div>
+                    <ul class="dropdown-menu">
+                    <li>
+                        <a href="#">Logout</a>
+                    </li>
+                    </ul>
+                </li>
+                
+            </ul>
+        </div>
     </div>
   </nav>
 </template>
--- resources/assets/js/router/index.js
+++ resources/assets/js/router/index.js
@@ -2,10 +2,12 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import { routes as routes } from '../app/index'
 
+console.log(routes);
+
 Vue.use(Router);
 
 const router = new Router({
-    routes: routes
+    routes
 });
 
 export default router;
